94b29b13d93d2187f1c512b2a02c8bb9f3e22a01
João Pacheco <e1526@itsector.pt>
2024-07-03 17:56:49 +0100

    [DEV] Args validation and logger

---
+++ JoaoPacheco_Veeam_Task/JoaoPacheco_Veeam_Task/Logger.cs
@@ -0,0 +1,19 @@
+﻿namespace JoaoPacheco_Veeam_Task
+{
+    public class Logger
+    {
+        public Logger(string logFilePath)
+        {
+            LogFilePath = logFilePath;
+        }
+
+        public string LogFilePath { get; private set; }
+
+        public void Log(string message)
+        {
+            string logMessage = DateTime.Now.ToString("HH:mm:ss") + ": " + message;
+            Console.WriteLine(logMessage);
+            File.AppendAllText(LogFilePath, logMessage + Environment.NewLine);
+        }
+    }
+}
--- JoaoPacheco_Veeam_Task/JoaoPacheco_Veeam_Task/Program.cs
+++ JoaoPacheco_Veeam_Task/JoaoPacheco_Veeam_Task/Program.cs
@@ -2,8 +2,58 @@
 {
     internal class Program
     {
+        private static Logger _logger;
+
         static void Main(string[] args)
         {
+            if (!ValidateArgs(args))
+                return;
+
+            _logger = new Logger(args[3]);
+        }
+
+        private static bool ValidateArgs(string[] args)
+        {
+            try
+            {
+                if (args.Length < 4)
+                {
+                    throw new ArgumentException("Not enough arguments provided. Expected: sourcePath, destinationPath, syncInterval, logFilePath.");
+                }
+
+                var sourcePath = args[0];
+                if (!Directory.Exists(sourcePath))
+                {
+                    throw new DirectoryNotFoundException($"Source directory does not exist: {sourcePath}");
+                }
+
+                var destinationPath = args[1];
+                if (!Directory.Exists(destinationPath))
+                {
+                    Directory.CreateDirectory(destinationPath);
+                    Console.WriteLine($"Destination directory created: {destinationPath}");
+                }
+
+                var syncIntervalSuccess = double.TryParse(args[2], out double syncInterval);
+                if (!syncIntervalSuccess)
+                {
+                    throw new FormatException($"Invalid sync interval: {args[2]}");
+                }
+
+                var logFilePath = args[3];
+                if (!File.Exists(logFilePath))
+                {
+                    File.Create(logFilePath).Close();
+                    Console.WriteLine($"Log file created: {logFilePath}");
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
         }
     }
 }

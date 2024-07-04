6a7fa8d1d3a86da83d3505742ca1cb47f00f7f24
João Pacheco <e1526@itsector.pt>
2024-07-04 09:51:51 +0100

    [TESTS] Added Tests project and Added basic tests to MD5 compare

---
+++ JoaoPacheco_Veeam_Task/JoaoPacheco_Veeam_TaskTests/HelperMethodsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace JoaoPacheco_Veeam_Task.Tests
+{
+    [TestClass()]
+    public class HelperMethodsTests
+    {
+        [TestMethod()]
+        public void CompareMD5TestFail()
+        {
+            var testFolder = Path.Combine(Directory.GetCurrentDirectory(), "TestFiles\\");
+            Assert.IsFalse(HelperMethods.CompareMD5(testFolder + "EqualFile1.txt", testFolder + "DifferentFile.txt"), "Different files have different MD5 hash");
+        }
+
+        [TestMethod()]
+        public void CompareMD5TestSuccess()
+        {
+            var testFolder = Path.Combine(Directory.GetCurrentDirectory(), "TestFiles\\");
+            Assert.IsTrue(HelperMethods.CompareMD5(testFolder + "EqualFile1.txt", testFolder + "EqualFile2.txt"), "Equal files have MD5 hash");
+        }
+    }
+}
